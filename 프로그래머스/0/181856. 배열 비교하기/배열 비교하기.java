79dfe6332931ce96a7622dfae991463287d95be5
Do-hyung111 <150571392+Do-hyung111@users.noreply.github.com>
2025-10-29 14:48:20 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -6,16 +6,11 @@ class Solution {
             return -1;
         else{
             int sum1 = 0, sum2 = 0;
-            for(int i:arr1)
-                sum1 += i;
-            for(int i:arr2)
-                sum2 += i;
-            if(sum1 > sum2)
-                return 1;
-            else if (sum1 < sum2)
-                return -1;
-            else
-                return 0;
+            for(int i=0; i<arr1.length;i++){
+                sum1 += arr1[i];
+                sum2 += arr2[i];
+            }
+            return sum1 == sum2 ? 0 : (sum1 > sum2 ? 1 : -1);
         }
     }
 }
