bbaff298838e821178e1b8ed15937b7832f3d79b
Do-hyung111 <150571392+Do-hyung111@users.noreply.github.com>
2025-11-10 08:03:46 +0900

    [level 0] Title: l로 만들기, Time: 15.92 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = myString;
-        
-        for(int i=0; i<answer.length();i++){
-            if(answer.charAt(i) < 'l'){
-                answer = answer.replace(""+answer.charAt(i), "l");
-            }
-        } 
-        return answer;
+        return myString.replaceAll("[a-k]", "l");
     }
 }
