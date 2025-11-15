1b64a8b6bf6aeff6225f7db414dbc90d64cb43fc
Do-hyung111 <150571392+Do-hyung111@users.noreply.github.com>
2025-11-15 09:18:01 +0900

    [level 1] Title: 자릿수 더하기, Time: 8.51 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String temp = n + "";
+        for(int i=0; i<temp.length();i++){
+            answer += Integer.parseInt(temp.charAt(i)+"");
+        }
+        return answer;
+    }
+}
