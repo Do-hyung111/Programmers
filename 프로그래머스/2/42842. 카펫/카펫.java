28caab75d1071e3e394332d802be4b9d765b5636
Do-hyung111 <150571392+Do-hyung111@users.noreply.github.com>
2025-12-21 12:26:26 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        for(int h = 1;h<=Math.sqrt(yellow);h++){
+            if(yellow%h == 0){
+                int w = yellow/h;
+                int width = w+2;
+                int height = h+2;
+            
+                if(height*width-yellow == brown){
+                    answer[0] = width;
+                    answer[1] = height;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
