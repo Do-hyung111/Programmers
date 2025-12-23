529d7c1271d6eb5d76df55f679a1e96bfaea6bc5
Do-hyung111 <150571392+Do-hyung111@users.noreply.github.com>
2025-12-23 21:19:18 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,8 +3,7 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for(int i=0; i<commands.length;i++){
-            int[] temp = new int[commands[i][1] - commands[i][0] + 1];
-            temp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            int[] temp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
             Arrays.sort(temp);            
             answer[i] = temp[commands[i][2]-1];
         }
