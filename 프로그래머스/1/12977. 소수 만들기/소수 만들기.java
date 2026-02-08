6008c8394d585c46290af75bdf1b4489a88829b6
Do-hyung111 <150571392+Do-hyung111@users.noreply.github.com>
2026-02-08 18:43:27 +0900

    [level 1] Title: 소수 만들기, Time: 2.49 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,31 +1,22 @@
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int sum = 0;
-        for(int i=0; i<nums.length-2;i++){
-            sum = nums[i];
-            for(int j=i+1;j<nums.length-1;j++){
-                sum += nums[j];
-                for(int k=j+1;k<nums.length;k++){
-                    sum+=nums[k];
-                    answer+=sosu(sum);
-                    System.out.println(sum);
-                    sum-=nums[k];
-                }
-                sum -= nums[j];
-            }
-        }
+        for(int i=0; i<nums.length-2;i++)
+            for(int j=i+1;j<nums.length-1;j++)
+                for(int k=j+1;k<nums.length;k++)
+                    answer += sosu(nums[i] + nums[j] + nums[k]);
         return answer;
     }
     
     public int sosu(int n){
-        int cnt = 0;
+        if(n<2)
+            return 0;
         
-        for(int i=1;i<n;i++)
+        for(int i=2;i*i<=n;i++)
             if(n%i==0)
-                cnt++;
+                return 0;
         
-        return (cnt > 1)?0:1;
+        return 1;
     }
     
 }
