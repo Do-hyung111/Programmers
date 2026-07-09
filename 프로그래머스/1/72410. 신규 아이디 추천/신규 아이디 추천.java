b583bcaa910d1fc93197f588ad9d776e793b8ba2
Do-hyung111 <150571392+Do-hyung111@users.noreply.github.com>
2026-07-09 16:41:24 +0900

    [level 1] Title: 신규 아이디 추천, Time: 5.20 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -4,22 +4,15 @@ class Solution {
         new_id = new_id.toLowerCase(); //step1: 소문자
         new_id = new_id.replaceAll("[^a-z|\\d|\\-|\\.|\\_]",""); //step2: 문자정리
         new_id = new_id.replaceAll("[//.]{2,}", "."); //step3: 연속 점 정리
-        new_id = deleteSpot(new_id); //step4: 첫,마지막 글자 정리
-        if(new_id.equals(""))
-            new_id = "a"; //step5: 빈 문자열 처리
-        if(new_id.length() >= 16)
-            new_id = deleteSpot(new_id.substring(0,15)); //step6: 크기 조절 
+        new_id = new_id.replaceAll("^[\\.]|[\\.]$",""); //step4: 첫,마지막 글자 정리
+        new_id = new_id.isEmpty() ? "a": new_id; //step5: 빈 문자열 처리
+        if(new_id.length() >= 16){
+            new_id = new_id.substring(0,15); //step6: 크기 조절 
+            new_id = new_id.replaceAll("[\\.]$","");
+        }
         while(new_id.length() <= 2)
             new_id += new_id.charAt(new_id.length()-1); //step7: 크기 조절
         
         return new_id;
-    }
-    
-    public String deleteSpot(String str){
-        if(str.length() > 0 && str.charAt(0) == '.')
-            str = str.substring(1);
-        if(str.length() > 0 && str.charAt(str.length()-1) == '.')
-            str = str.substring(0,str.length()-1);
-        return str;
     }
 }
